7e88cf3ccdf2dbb8c31c495a459d3db784422cc5
Callum Moseley <callum.moseley1@gmail.com>
2021-07-13 19:10:18 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use http::Uri;
 use ini::Ini;
 use native_tls::TlsConnector;
 use reqwest;
+use schema::assign_vulcast_to_relay::AssignVulcastToRelayAssignVulcastToRelay::{
+    AuthenticationError, RelayAssignment, VulcastAssignedToRelayError,
+};
+use schema::log_in_as_vulcast::LogInAsVulcastLogInAsVulcast::{
+    AuthenticationError as LoginAuthenticationError, VulcastAuthentication,
+};
 use serde::Serialize;
 use tokio::net::TcpStream;
 use tokio_tungstenite::Connector;
@@ -45,16 +51,16 @@ async fn login(conf: &Ini, client: &reqwest::Client) -> Result<String> {
         .data
         .ok_or(anyhow!("Request returned no data"))?;
     match response_data.log_in_as_vulcast {
-        schema::log_in_as_vulcast::LogInAsVulcastLogInAsVulcast::VulcastAuthentication(auth) => {
-            Ok(auth.vulcast_access_token)
-        }
-        schema::log_in_as_vulcast::LogInAsVulcastLogInAsVulcast::AuthenticationError(error) => {
-            Err(anyhow!("Authentication error: {}", error.message))
-        }
+        VulcastAuthentication(auth) => Ok(auth.vulcast_access_token),
+        LoginAuthenticationError(error) => Err(anyhow!("Authentication error: {}", error.message)),
     }
 }
 
-async fn assign_relay(conf: &Ini, client: &reqwest::Client, auth_token: &str) -> Result<String> {
+async fn assign_relay(
+    conf: &Ini,
+    client: &reqwest::Client,
+    auth_token: &str,
+) -> Result<(String, String)> {
     log::info!("Requesting relay assignment");
 
     let uri = conf
@@ -80,13 +86,11 @@ async fn assign_relay(conf: &Ini, client: &reqwest::Client, auth_token: &str) ->
         .data
         .ok_or(anyhow!("Request returned no data"))?;
     match response_data.assign_vulcast_to_relay {
-        schema::assign_vulcast_to_relay::AssignVulcastToRelayAssignVulcastToRelay::RelayAssignment(assignment) => {
-            Ok(assignment.relay_access_token)
+        RelayAssignment(assignment) => {
+            Ok((assignment.relay.host_name, assignment.relay_access_token))
         }
-        schema::assign_vulcast_to_relay::AssignVulcastToRelayAssignVulcastToRelay::AuthenticationError(error) => {
-            Err(anyhow!("Authentication error: {}", error.message))
-        }
-        schema::assign_vulcast_to_relay::AssignVulcastToRelayAssignVulcastToRelay::VulcastAssignedToRelayError(error) => {
+        AuthenticationError(error) => Err(anyhow!("Authentication error: {}", error.message)),
+        VulcastAssignedToRelayError(error) => {
             Err(anyhow!("Vulcast already assigned error: {}", error.message))
         }
     }
@@ -101,28 +105,30 @@ async fn main() -> Result<()> {
     let client = reqwest::Client::new();
 
     let access_token = login(&conf, &client).await?;
-    let relay_token = assign_relay(&conf, &client, &access_token).await?;
+    let (relay_host, relay_token) = assign_relay(&conf, &client, &access_token).await?;
+    let relay_host = "192.168.0.180".to_owned();
 
-    log::info!("{:?}", relay_token);
+    log::info!("Assigned to relay {:?}", relay_host);
+
+    let port: u16 = conf
+        .get_from(Some("network"), "signal_port")
+        .expect("Signal port not specified")
+        .parse()
+        .expect("Signal port could not be parsed as an int");
+    let relay_uri: Uri = format!("wss://{}:{}", relay_host, port).parse().unwrap();
+
+    log::info!("{:?}", relay_uri);
+
+    let stream = TcpStream::connect((relay_host, port)).await?;
+    let req = http::Request::builder()
+        .uri(relay_uri)
+        .header("Sec-WebSocket-Protocol", "graphql-ws")
+        .body(())?;
 
     let connector = TlsConnector::builder()
         .danger_accept_invalid_hostnames(true)
         .danger_accept_invalid_certs(true)
         .build()?;
-    let uri: Uri = conf
-        .get_from(Some("network"), "signal_addr")
-        .expect("No signal address specified")
-        .parse()?;
-    log::info!("connecting to {}", &uri);
-
-    let host = uri.host().unwrap();
-    let port = uri.port_u16().unwrap();
-    let stream = TcpStream::connect((host, port)).await?;
-
-    let req = http::Request::builder()
-        .uri(uri)
-        .header("Sec-WebSocket-Protocol", "graphql-ws")
-        .body(())?;
     let (socket, _response) = tokio_tungstenite::client_async_tls_with_config(
         req,
         stream,
@@ -130,6 +136,8 @@ async fn main() -> Result<()> {
         Some(Connector::NativeTls(connector)),
     )
     .await?;
+
+    log::info!("hi");
 
     let mut ws_client = GraphQLWebSocket::new();
     ws_client.connect(socket, Some(serde_json::to_value(relay_token)?));
